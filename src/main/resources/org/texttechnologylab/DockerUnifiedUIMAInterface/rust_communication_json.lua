ac12c0831a92d66c6866f69fdf0e48b3016e00d3
Alexander Leonhardt <equinox.salexander@gmail.com>
2022-04-13 11:47:51 +0200

    Trying to fix spurious error again

--- src/main/resources/org/texttechnologylab/DockerUnifiedUIMAInterface/rust_communication_json.lua
+++ src/main/resources/org/texttechnologylab/DockerUnifiedUIMAInterface/rust_communication_json.lua
@@ -4,21 +4,24 @@ util = luajava.bindClass("org.apache.uima.fit.util.JCasUtil")
 
 -- Serialize all tokens as performance test
 function serialize(inputCas,outputStream)
-  beginsent = luajava.newInstance("org.json.JSONArray")
-  endsent = luajava.newInstance("org.json.JSONArray")
-  send = luajava.newInstance("org.json.JSONArray")
+  local beginsent = luajava.newInstance("org.json.JSONArray")
+  local endsent = luajava.newInstance("org.json.JSONArray")
+  local send = luajava.newInstance("org.json.JSONArray")
   
   local result = util:select(inputCas,token):iterator()
   while result:hasNext() do
     local x = result:next()
-	  beginsent:put(x:getBegin())
-      endsent:put(x:getEnd())
+	beginsent:put(x:getBegin())
+    endsent:put(x:getEnd())
   end
+
   send:put(inputCas:getDocumentText())
   send:put(beginsent)
+
   send:put(endsent)
   -- Differentiate function call to prevent best effort lookup
-  outputStream:write(send:toString(0))
+  local ok = send:toString(0)
+  outputStream:write(ok)
 end
 
 function deserialize(inputCas,inputStream)
