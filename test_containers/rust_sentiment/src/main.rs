bcf63cf49e5c9662275f0359e44c7a6aefe2151c
Alexander Leonhardt <equinox.salexander@gmail.com>
2022-04-13 13:23:31 +0200

    Added fixes for the rust implementation

--- test_containers/rust_sentiment/src/main.rs
+++ test_containers/rust_sentiment/src/main.rs
@@ -2,6 +2,7 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
+    let lua : String = std::fs::read_to_string("rust_communication_msgpack.lua").unwrap();
     let hello = warp::path!("v1"/"process")
         .map(|| {
             "Hello!"
@@ -9,8 +10,8 @@ async fn main() {
         .or(warp::path!("v1"/"typesystem").map(|| {
             "Hello typesystem"
         }))
-        .or(warp::path!("v1"/"communication_layer").map(|| {
-            "Hello communication!"
+        .or(warp::path!("v1"/"communication_layer").map(move  || {
+            lua.clone()
         }));
 
     warp::serve(hello)
