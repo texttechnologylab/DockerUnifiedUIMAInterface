9d422faed71b5e80efc6d5ee7ef229546247ba51
abrami <abrami@em.uni-frankfurt.de>
2022-06-13 11:26:19 +0200

    small adjustments adding ProgressMeter and shutdown Hook to AsyncCollectionReader

--- src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/io/AsyncCollectionReader.java
+++ src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/io/AsyncCollectionReader.java
@@ -1,5 +1,6 @@
 package org.texttechnologylab.DockerUnifiedUIMAInterface.io;
 
+import de.tudarmstadt.ukp.dkpro.core.api.io.ProgressMeter;
 import org.apache.commons.compress.compressors.CompressorException;
 import org.apache.commons.compress.compressors.CompressorInputStream;
 import org.apache.commons.compress.compressors.CompressorStreamFactory;
@@ -20,7 +21,15 @@ public class AsyncCollectionReader {
     private int _initialSize;
     private AtomicInteger _docNumber;
 
+    private ProgressMeter progress = null;
+
+    private int debugCount = 25;
+
     public AsyncCollectionReader(String folder, String ending) {
+        this(folder, ending, 25);
+    }
+
+    public AsyncCollectionReader(String folder, String ending, int debugCount) {
         File fl = new File(folder);
         if(!fl.isDirectory()) {
             throw new RuntimeException("The folder is not a directory!");
@@ -33,13 +42,26 @@ public class AsyncCollectionReader {
         System.out.printf("Found %d files matching the pattern!\n",_filePaths.size());
         _initialSize = _filePaths.size();
         _docNumber = new AtomicInteger(0);
+
+        progress = new ProgressMeter(_initialSize);
     }
 
     public boolean getNextCAS(JCas empty) throws IOException, CompressorException, SAXException {
         String result = _filePaths.poll();
         if(result==null) return false;
         int val = _docNumber.addAndGet(1);
-        System.out.printf("Loading %d/%d\n",val,_initialSize);
+
+        progress.setDone(val);
+        progress.setLeft(_initialSize-val);
+
+        if(_initialSize-progress.getCount()>debugCount) {
+            if (val % debugCount == 0 || val == 1) {
+                System.out.printf("%s: %s\n", progress, result);
+            }
+        }
+        else{
+            System.out.printf("%s: %s\n", progress, result);
+        }
 
         byte []file = Files.readAllBytes(Path.of(result));
         InputStream decodedFile;
