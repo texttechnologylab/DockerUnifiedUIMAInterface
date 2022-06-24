61580a7e3d71f20eca1de56d73df78d153ec1269
Givara <givaraebo>
2022-06-24 15:34:31 +0200

    socketIO is installed

--- src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/connection/IDUUIConnectionHandler.java
+++ src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/connection/IDUUIConnectionHandler.java
@@ -4,10 +4,11 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 public interface IDUUIConnectionHandler {
-
+    SocketIO socketIO = null;
     void initiate(String uri) throws URISyntaxException;
 
     boolean success();
 
     byte[] sendAwaitResponse(byte[] serializedObject) throws IOException;
+
 }
