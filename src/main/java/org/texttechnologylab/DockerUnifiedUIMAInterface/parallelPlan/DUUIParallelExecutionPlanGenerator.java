3b6b3014f9c5f03055dfb4148fc7bb11106bc558
M8192 <online-m-r@web.de>
2022-09-06 00:48:44 +0200

    caching for InputsOutputs

--- src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/parallelPlan/DUUIParallelExecutionPlanGenerator.java
+++ src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/parallelPlan/DUUIParallelExecutionPlanGenerator.java
@@ -2,18 +2,26 @@ package org.texttechnologylab.DockerUnifiedUIMAInterface.parallelPlan;
 
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.texttechnologylab.DockerUnifiedUIMAInterface.DUUIComposer;
 import org.texttechnologylab.DockerUnifiedUIMAInterface.IDUUIExecutionPlan;
 import org.texttechnologylab.DockerUnifiedUIMAInterface.IDUUIExecutionPlanGenerator;
+import org.texttechnologylab.DockerUnifiedUIMAInterface.InputsOutputs;
 
 import java.util.*;
 
 public class DUUIParallelExecutionPlanGenerator implements IDUUIExecutionPlanGenerator {
 
     private final Collection<DUUIComposer.PipelinePart> pipelineParts;
+    private final Map<String, InputsOutputs> part2IO;
 
-    public DUUIParallelExecutionPlanGenerator(Collection<DUUIComposer.PipelinePart> pipelineParts) {
+    public DUUIParallelExecutionPlanGenerator(Collection<DUUIComposer.PipelinePart> pipelineParts) throws ResourceInitializationException {
         this.pipelineParts = pipelineParts;
+        // cache input and output
+        part2IO = new TreeMap<>();
+        for (DUUIComposer.PipelinePart part : pipelineParts)
+            part2IO.put(part.getUUID(), part.getDriver().getInputsOutputs(part.getUUID()));
     }
 
     @Override
@@ -31,26 +39,18 @@ public class DUUIParallelExecutionPlanGenerator implements IDUUIExecutionPlanGen
         // build a Map that maps from output to ParallelExecutionPlan
         Map<String, Set<DUUIParallelExecutionPlan>> satisfiesToPipelinePart = new HashMap<>();
         for (DUUIParallelExecutionPlan plan : remaining) {
-            try {
-                for (String s : plan.getPipelinePart().getDriver().getInputsOutputs(plan.getPipelinePart().getUUID()).getOutputs()) {
-                    if (satisfiesToPipelinePart.containsKey(s))
-                        satisfiesToPipelinePart.get(s).add(plan);
-                    else
-                        satisfiesToPipelinePart.put(s, new HashSet<>(Collections.singletonList(plan)));
-                }
-            } catch (ResourceInitializationException e) {
-                throw new RuntimeException(e);
+            for (String s : part2IO.get(plan.getPipelinePart().getUUID()).getOutputs()) {
+                if (satisfiesToPipelinePart.containsKey(s))
+                    satisfiesToPipelinePart.get(s).add(plan);
+                else
+                    satisfiesToPipelinePart.put(s, new HashSet<>(Collections.singletonList(plan)));
             }
         }
 
         // plans with no inputs are processed after root
         for (DUUIParallelExecutionPlan plan : remaining) {
-            try {
-                if (plan.getPipelinePart().getDriver().getInputsOutputs(plan.getPipelinePart().getUUID()).getInputs().size() == 0) {
-                    root.addNext(plan);
-                }
-            } catch (ResourceInitializationException e) {
-                throw new RuntimeException(e);
+            if (part2IO.get(plan.getPipelinePart().getUUID()).getInputs().size() == 0) {
+                root.addNext(plan);
             }
         }
 
@@ -58,20 +58,16 @@ public class DUUIParallelExecutionPlanGenerator implements IDUUIExecutionPlanGen
             for (Iterator<DUUIParallelExecutionPlan> iterator = remaining.iterator(); iterator.hasNext(); ) {
                 DUUIParallelExecutionPlan plan = iterator.next();
 
-                try {
-                    if (satisfied.containsAll(plan.getPipelinePart().getDriver().getInputsOutputs(plan.getPipelinePart().getUUID()).getInputs())) {
-                        // run
-                        for (String inputs : plan.getPipelinePart().getDriver().getInputsOutputs(plan.getPipelinePart().getUUID()).getInputs())
-                            for (DUUIParallelExecutionPlan requiredPlan : satisfiesToPipelinePart.get(inputs)) {
-                                requiredPlan.addNext(plan);
-                                plan.addPrevious(requiredPlan);
-                            }
+                if (satisfied.containsAll(part2IO.get(plan.getPipelinePart().getUUID()).getInputs())) {
+                    // run
+                    for (String inputs : part2IO.get(plan.getPipelinePart().getUUID()).getInputs())
+                        for (DUUIParallelExecutionPlan requiredPlan : satisfiesToPipelinePart.get(inputs)) {
+                            requiredPlan.addNext(plan);
+                            plan.addPrevious(requiredPlan);
+                        }
 
-                        satisfied.addAll(plan.getPipelinePart().getDriver().getInputsOutputs(plan.getPipelinePart().getUUID()).getOutputs());
-                        iterator.remove();
-                    }
-                } catch (ResourceInitializationException e) {
-                    throw new RuntimeException(e);
+                    satisfied.addAll(part2IO.get(plan.getPipelinePart().getUUID()).getOutputs());
+                    iterator.remove();
                 }
             }
         }
