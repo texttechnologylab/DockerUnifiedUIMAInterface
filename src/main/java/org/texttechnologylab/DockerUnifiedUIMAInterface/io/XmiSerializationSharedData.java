339df0f917e820af5dc3c1d475ed4880c1e76b02
Giuseppe Abrami <abrami@em.uni-frankfurt.de>
2023-05-12 17:08:31 +0200

    update Serializer

--- src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/io/XmiSerializationSharedData.java
+++
@@ -1,7 +0,0 @@
-package org.texttechnologylab.DockerUnifiedUIMAInterface.io;
-
-import java.io.Serializable;
-
-public class XmiSerializationSharedData extends org.apache.uima.cas.impl.XmiSerializationSharedData implements Serializable {
-
-}
