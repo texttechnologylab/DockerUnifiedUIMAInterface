0f1943de15ae8ea9f50f2d4ba6fc6655c1da4eb6
Daniel Baumartz <baumartz@stud.uni-frankfurt.de>
2023-05-15 12:34:16 +0200

    WIP Segmentation

--- src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/segmentation/DUUISegmentationStrategy.java
+++ src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/segmentation/DUUISegmentationStrategy.java
@@ -1,11 +1,25 @@
 package org.texttechnologylab.DockerUnifiedUIMAInterface.segmentation;
 
-import org.apache.uima.UIMAException;
 import org.apache.uima.jcas.JCas;
 
-import java.util.List;
+public abstract class DUUISegmentationStrategy implements Iterable<JCas> {
+    // The current JCas to be processed
+    protected JCas jCas;
 
-public abstract class DUUISegmentationStrategy implements IDUUISegmentationStrategy {
-    public abstract List<JCas> segment(JCas jCas) throws UIMAException;
-    public abstract void combine(List<JCas> jCasSegmenteds, JCas jCas);
+    // Set the JCas to be processed, this should reset all state
+    public void setJCas(JCas jCas) {
+        this.jCas = jCas;
+        this.initialize();
+    }
+
+    public JCas getJCas() {
+        return jCas;
+    }
+
+    // Initialize the state of the segmentation strategy,
+    // this is called when a new JCas is set
+    protected abstract void initialize();
+
+    // Merge the segmented JCas back into the original JCas
+    public abstract void merge(JCas jCasSegment);
 }
