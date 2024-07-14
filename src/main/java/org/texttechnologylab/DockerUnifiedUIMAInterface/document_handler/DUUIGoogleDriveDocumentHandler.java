d33ad5562ea1105c10404392dd2d3be165683e43
dterefe <daveterefe@gmail.com>
2024-07-14 20:33:20 +0200

    Changed GoogleDriveHandler Constructor to accept credentials directly.

--- src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/document_handler/DUUIGoogleDriveDocumentHandler.java
+++ src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/document_handler/DUUIGoogleDriveDocumentHandler.java
@@ -1,5 +1,6 @@
 package org.texttechnologylab.DockerUnifiedUIMAInterface.document_handler;
 
+import com.google.api.client.auth.oauth2.BearerToken;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
@@ -26,7 +27,7 @@ import java.util.stream.Collectors;
 
 public class DUUIGoogleDriveDocumentHandler implements IDUUIDocumentHandler, IDUUIFolderPickerApi {
 
-    private static final String APPLICATION_NAME = "DriveDocumentHandler";
+    private static final String APPLICATION_NAME = "DUUI Controller";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final String TOKENS_DIRECTORY_PATH = "tokens";
 
@@ -37,50 +38,24 @@ public class DUUIGoogleDriveDocumentHandler implements IDUUIDocumentHandler, IDU
     private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
     private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
-    private Drive service;
-
-    public DUUIGoogleDriveDocumentHandler() throws GeneralSecurityException, IOException {
+    private final Drive service;
+    public DUUIGoogleDriveDocumentHandler(Credential credential) throws GeneralSecurityException, IOException {
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+        service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
                 .setApplicationName(APPLICATION_NAME)
                 .build();
     }
 
-
-    /**
-     * Creates an authorized Credential object.
-     * @param HTTP_TRANSPORT The network HTTP Transport.
-     * @return An authorized Credential object.
-     * @throws IOException If the credentials.json file cannot be found.
-     */
-    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
-        // Load client secrets.
-        InputStream in = DUUIGoogleDriveDocumentHandler.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-        if (in == null) {
-            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
-        }
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("online")
-                .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-    }
-
     public static void main(String... args) throws IOException, GeneralSecurityException {
 
-        DUUIGoogleDriveDocumentHandler handler = new DUUIGoogleDriveDocumentHandler();
+//        DUUIGoogleDriveDocumentHandler handler = new DUUIGoogleDriveDocumentHandler();
 //        DUUIDocument doc = handler.readDocument(handler.getFileId("firstpdf.pdf"));
 //
 //        doc.setName("secondpdf.pdf");
 //
 //        handler.writeDocument(doc, handler.getFolderId("first"));
 
-        System.out.println(handler.getFolderStructure().toJson().toString());
+//        System.out.println(handler.getFolderStructure().toJson().toString());
 
     }
 
