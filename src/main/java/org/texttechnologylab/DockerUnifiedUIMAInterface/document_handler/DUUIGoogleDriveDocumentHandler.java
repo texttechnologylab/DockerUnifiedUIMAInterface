bf5df3d85186150ed47b6a50881e112b61dae82f
dterefe <daveterefe@gmail.com>
2024-07-15 18:49:03 +0200

    Fixes.

--- src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/document_handler/DUUIGoogleDriveDocumentHandler.java
+++ src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/document_handler/DUUIGoogleDriveDocumentHandler.java
@@ -1,19 +1,12 @@
 package org.texttechnologylab.DockerUnifiedUIMAInterface.document_handler;
 
-import com.google.api.client.auth.oauth2.BearerToken;
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.InputStreamContent;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
 
@@ -21,7 +14,6 @@ import java.io.*;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 
@@ -29,14 +21,6 @@ public class DUUIGoogleDriveDocumentHandler implements IDUUIDocumentHandler, IDU
 
     private static final String APPLICATION_NAME = "DUUI Controller";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
-
-    /**
-     * Global instance of the scopes required by this quickstart.
-     * If modifying these scopes, delete your previously saved tokens/ folder.
-     */
-    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
-    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
     private final Drive service;
     public DUUIGoogleDriveDocumentHandler(Credential credential) throws GeneralSecurityException, IOException {
