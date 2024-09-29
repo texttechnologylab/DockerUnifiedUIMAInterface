0f118408c08c6f116c28455ff6df2f6eaf2b133a
Mocretion <mocretion@gmail.com>
2024-09-29 16:21:39 +0200

    removed l due to parsing error

--- src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/driver/DUUIPipelineComponent.java
+++ src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/driver/DUUIPipelineComponent.java
@@ -665,6 +665,6 @@ public class DUUIPipelineComponent {
     }
 
     public long getTimeout() {
-        return Long.valueOf(_options.getOrDefault(timeout, "60l"));
+        return Long.valueOf(_options.getOrDefault(timeout, "60"));
     }
 }
