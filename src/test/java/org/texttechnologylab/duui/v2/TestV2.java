937f933652ba10247c6cd3ee31ff7716fa761cb9
Manuel Schaaf <manuel.schaaf@em.uni-frankfurt.de>
2025-04-28 17:05:24 +0200

    feat(lua-process): Improved tests for spacy-v2 component

--- src/test/java/org/texttechnologylab/duui/v2/TestV2.java
+++ src/test/java/org/texttechnologylab/duui/v2/TestV2.java
@@ -9,6 +9,7 @@ import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.cas.FSArray;
 import org.apache.uima.jcas.tcas.Annotation;
 import org.junit.Test;
 import org.texttechnologylab.DockerUnifiedUIMAInterface.DUUIComposer;
@@ -16,14 +17,13 @@ import org.texttechnologylab.DockerUnifiedUIMAInterface.driver.DUUIDockerDriver;
 import org.texttechnologylab.DockerUnifiedUIMAInterface.driver.DUUIRemoteDriver;
 import org.texttechnologylab.DockerUnifiedUIMAInterface.driver.DUUIUIMADriver;
 import org.texttechnologylab.DockerUnifiedUIMAInterface.lua.DUUILuaContext;
+import org.texttechnologylab.annotation.SpacyAnnotatorMetaData;
 
 import java.net.URISyntaxException;
 
 public class TestV2 {
     @Test
-    public void test() throws Exception {
-
-
+    public void test_with_sentences() throws Exception {
         DUUIComposer composer = new DUUIComposer()
                 .withLuaContext(
                         new DUUILuaContext().withJsonLibrary()
@@ -32,9 +32,13 @@ public class TestV2 {
                 .addDriver(new DUUIRemoteDriver())
                 .addDriver(new DUUIDockerDriver());
 
-        composer.add(new DUUIDockerDriver.Component(
-                "duui-spacy-v2:dev"
-        ));
+        composer.add(
+                new DUUIDockerDriver.Component(
+                        "duui-spacy-v2:dev"
+                )
+                        .withParameter("spacy_model_size", "lg")
+                        .build()
+        );
 
         JCas jCas = JCasFactory.createJCas();
         jCas.setDocumentText(
@@ -45,7 +49,57 @@ public class TestV2 {
         new Sentence(jCas, 0, 104).addToIndexes();
         new Sentence(jCas, 106, 177).addToIndexes();
 
-        composer.run(jCas, "name");
+        composer.run(jCas, "lua-process-test");
+
+        printResult(jCas);
+    }
+
+    @Test
+    public void test_wo_sentences() throws Exception {
+        DUUIComposer composer = new DUUIComposer()
+                .withLuaContext(
+                        new DUUILuaContext().withJsonLibrary()
+                )
+                .withSkipVerification(true)
+                .addDriver(new DUUIRemoteDriver())
+                .addDriver(new DUUIDockerDriver());
+
+        composer.add(
+                new DUUIDockerDriver.Component(
+                        "duui-spacy-v2:dev"
+                )
+                        .withParameter("spacy_model_size", "sm")
+                        .build()
+        );
+
+        JCas jCas = JCasFactory.createJCas();
+        jCas.setDocumentText(
+                "Die Goethe Universität ist auf vier große Universitätsgelände über das Frankfurter Stadtgebiet verteilt.\n "
+                        + "Barack Obama war der 44. Präsident der Vereinigten Staaten von Amerika."
+        );
+        jCas.setDocumentLanguage("de");
+
+        composer.run(jCas, "lua-process-test");
+
+        printResult(jCas);
+    }
+
+    private static void printResult(JCas jCas) {
+        System.out.println("### SpacyAnnotatorMetaData ###");
+        for (SpacyAnnotatorMetaData annotation : JCasUtil.select(jCas, SpacyAnnotatorMetaData.class)) {
+            StringBuilder sb = new StringBuilder();
+            annotation.prettyPrint(0, 2, sb, true);
+            System.out.println(sb);
+            System.out.println();
+        }
+
+        System.out.println("### Sentence ###");
+        for (Sentence annotation : JCasUtil.select(jCas, Sentence.class)) {
+            StringBuilder sb = new StringBuilder();
+            annotation.prettyPrint(0, 2, sb, true);
+            System.out.println(sb);
+            System.out.println();
+        }
 
         System.out.println("### Token ###");
         for (Token annotation : JCasUtil.select(jCas, Token.class)) {
