ad07b1fe2f2593b53700db599973ba246649a6d4
Manuel Schaaf <manuel.schaaf@em.uni-frankfurt.de>
2025-04-28 11:28:21 +0200

    feat(lua-process): Added methods to set HTTP request headers

--- src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/driver/DUUIHttpRequestHandler.java
+++ src/main/java/org/texttechnologylab/DockerUnifiedUIMAInterface/driver/DUUIHttpRequestHandler.java
@@ -105,5 +105,73 @@ public class DUUIHttpRequestHandler {
         return sendAsync(request);
     }
 
+    /**
+     * Adds the given name-value pair to the set of headers for this request
+     *
+     * @param name  the header name
+     * @param value the header value
+     * @see HttpRequest.Builder#header
+     */
+    public void header(String name, String value) {
+        this.builder.header(name, value);
+    }
+
+    /**
+     * Adds the given name-value pairs to the set of headers for this request.
+     *
+     * @param headers the list of name-value pairs
+     * @see HttpRequest.Builder#headers
+     */
+    public void headers(String... headers) {
+        this.builder.headers(headers);
+    }
+
+    /**
+     * Set the given header to the given value. Overwrites previously set values.
+     *
+     * @param name  the name of the header to set
+     * @param value the value of the header
+     * @see HttpRequest.Builder#setHeader
+     */
+    public void setHeader(String name, String value) {
+        this.builder.setHeader(name, value);
+    }
+
+    /**
+     * Adds the given name-value pairs to the set of headers for this request.
+     * <p/>
+     * Equivalent to {@link #setHeader(String, String) headers.forEach(handler::setHeader)}.
+     *
+     * @param headers a {@link Map} of name-value pairs
+     * @see HttpRequest.Builder#headers
+     */
+    public void setHeaders(Map<String, String> headers) {
+        headers.forEach(this::setHeader);
+    }
+
+    /**
+     * Create a copy of this request handler with the given headers set.
+     *
+     * @param headers a list of name-value header pairs.
+     * @return the new request handler.
+     * @see DUUIHttpRequestHandler#headers
+     */
+    public DUUIHttpRequestHandler withHeaders(String... headers) {
+        DUUIHttpRequestHandler clone = new DUUIHttpRequestHandler(this.client, this.url, this.builder.copy());
+        clone.headers(headers);
+        return clone;
+    }
+
+    /**
+     * Create a copy of this request handler with the given headers set.
+     *
+     * @param headers a map of name-value header pairs.
+     * @return the new request handler.
+     * @see DUUIHttpRequestHandler#headers
+     */
+    public DUUIHttpRequestHandler withHeaders(Map<String, String> headers) {
+        DUUIHttpRequestHandler clone = new DUUIHttpRequestHandler(this.client, this.url, this.builder.copy());
+        headers.forEach(clone::setHeader);
+        return clone;
     }
 }
